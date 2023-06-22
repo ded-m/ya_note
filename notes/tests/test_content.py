65afac62f80cae226879bbb5c67c2449bd8bbd4b
ded <89196994468@yandex.ru>
2023-06-22 08:54:30 +0300

    test_routes_OK

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,9 +1,6 @@
-from django.conf import settings
 from django.test import TestCase
-
+from unittest import skip
 from django.urls import reverse
-from datetime import datetime, timedelta
-from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 from notes.models import Note
@@ -11,32 +8,31 @@ from notes.models import Note
 User = get_user_model()
 
 
+@skip('Пропущено')
 class TestNotesPage(TestCase):
-    # Вынесем ссылку на домашнюю страницу в атрибуты класса.
+    # Вынесем ссылку на страницу заметок в атрибуты класса.
     NOTES_URL = reverse('notes:list')
 
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Лев Толстой')
         # Вычисляем текущую дату.
-        today = datetime.today()
         all_notes = [
             Note(
                 title=f'Заметка {index}',
                 text=f'Текст заметки {index}.',
-                # Для каждой заметки изменяем slug на index дней от today,
+                # Для каждой заметки изменяем slug,
                 # где index - счётчик цикла.
-                slug='notes' + str(today - timedelta(days=index)),
+                slug=f'notes_{index}',
                 author=cls.author
             )
             for index in range(11)
         ]
-        Note.objects.bulk_create(all_notes) 
+        Note.objects.bulk_create(all_notes)
 
     def test_notes_count(self):
         # Загружаем страницу с заметками.
         response = self.client.get(self.NOTES_URL)
-        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
         # Получаем список объектов из словаря контекста.
         object_list = response.context['object_list']
         # Определяем длину списка.
@@ -44,20 +40,20 @@ class TestNotesPage(TestCase):
         # Проверяем, что на странице именно 10 новостей.
         self.assertEqual(notes_count, 10)
 
-
+@skip('Пропущено')
 class TestDetailPage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Лев Толстой')
-        cls.notes = Note.objects.create(
+        cls.note = Note.objects.create(
             title='Тестовая заметка',
             text='Текст заметки.',
             author=cls.author,
             slug='note_slug'
         )
         # Сохраняем в переменную адрес страницы с новостью:
-        cls.detail_url = reverse('notes:detail', args=(cls.notes.slug,))
+        cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
 
     def test_anonymous_client_has_no_form(self):
         response = self.client.get(self.detail_url)
--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -1,29 +1,26 @@
-# news/tests/test_routes.py
-# Импортируем класс HTTPStatus.
 from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-# from unittest import skip
-# Импортируем функцию reverse().
+from unittest import skip
 from django.urls import reverse
-# Импортируем класс модели заметок.
 from notes.models import Note
 
 
 User = get_user_model()
 
 
-# @skip('Проверено')
+@skip('Проверено')
 class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         # Создаём пользователя:
         cls.author = User.objects.create(username='Лев Толстой')
-        
-        # Создаём заметку:
-        cls.note = Note.objects.create(title='Заголовок', text='Текст', author=cls.author)
 
+        # Создаём заметку:
+        cls.note = Note.objects.create(title='Заголовок',
+                                       text='Текст',
+                                       author=cls.author)
 
     def test_pages_availability(self):
         urls = (
@@ -38,7 +35,6 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
-
 
     def test_redirect_for_anonymous_client(self):
         # Сохраняем адрес страницы логина:
@@ -63,3 +59,20 @@ class TestRoutes(TestCase):
                 response = self.client.get(url)
                 # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url)
+
+    def test_pages_availability_for_authorized_user(self):
+        urls = (
+            ('notes:add', None),
+            ('notes:detail', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+            ('notes:list', None),
+            ('notes:success', None),
+        )
+        # Логиним пользователя в клиенте:
+        self.client.force_login(self.author)
+
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
