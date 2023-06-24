59deb518c89fadd40b461cfddb2299ce46e97ca4
ded <89196994468@yandex.ru>
2023-06-24 12:49:07 +0300

    all_tests-OK

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -14,28 +14,64 @@ class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        # Создаём пользователя:
+        # Создаём автора:
         cls.author = User.objects.create(username='Лев Толстой')
+        # Создаём читателя:
+        cls.reader = User.objects.create(username='Другой пользователь')
 
         # Создаём заметку:
         cls.note = Note.objects.create(title='Заголовок',
                                        text='Текст',
                                        author=cls.author)
 
-    def test_pages_availability(self):
+    def test_pages_availability_for_anonymous_user(self):
         """Страницы доступны незарегистрированному пользователю"""
-        urls = (
-            ('notes:home', None),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
-
-        )
-        for name, args in urls:
+        names = ('notes:home', 'users:login', 'users:logout', 'users:signup')
+        for name in names:
             with self.subTest(name=name):
+                url = reverse(name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_pages_availability_for_auth_user(self):
+        """Страницы доступны зарегистрированному пользователю"""
+        names = ('notes:home', 'users:login', 'users:logout', 'users:signup')
+        # Логиним пользователя в клиенте:
+        self.client.force_login(self.author)
+
+        for name in names:
+            with self.subTest(name=name):
+                url = reverse(name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_pages_availability_for_author(self):
+        """Страницы доступны зарегистрированному пользователю"""
+        names = ('notes:detail', 'notes:edit', 'notes:delete')
+        args = (self.note.slug,)
+        # Логиним автора в клиенте:
+        self.client.force_login(self.author)
+        for name in names:
+            with self.subTest(name=name, args=args):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_pages_availability_for_different_users(self):
+        names = ('notes:detail', 'notes:edit', 'notes:delete')
+        users = (self.author, self.reader)
+        args = (self.note.slug,)
+        for user in users:
+            for name in names:
+                with self.subTest(user=user, name=name, args=args):
+                    # Логиним пользователя в клиенте:
+                    self.client.force_login(user)
+                    url = reverse(name, args=args)
+                    response = self.client.get(url)
+                    if user == self.author:
+                        self.assertEqual(response.status_code, HTTPStatus.OK)
+                    else:
+                        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_redirect_for_anonymous_client(self):
         """Страницы не доступны незарегистрированному пользователю"""
@@ -62,21 +98,3 @@ class TestRoutes(TestCase):
                 response = self.client.get(url)
                 # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url)
-
-    def test_pages_availability_for_authorized_user(self):
-        """Страницы доступны зарегистрированному пользователю"""
-        urls = (
-            ('notes:add', None),
-            ('notes:detail', (self.note.slug,)),
-            ('notes:delete', (self.note.slug,)),
-            ('notes:list', None),
-            ('notes:success', None),
-        )
-        # Логиним пользователя в клиенте:
-        self.client.force_login(self.author)
-
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
